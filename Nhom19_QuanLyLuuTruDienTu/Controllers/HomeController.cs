0d78e2794a49cce59c98d9b66b6e13b69eaaf42a
thaihung2510 <thchannel1258vn@gmail.com>
2022-04-22 03:07:21 +0700

    fix UI, model

--- Nhom19_QuanLyLuuTruDienTu/Controllers/HomeController.cs
+++ Nhom19_QuanLyLuuTruDienTu/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+
 
 namespace Nhom19_QuanLyLuuTruDienTu.Controllers
 {
@@ -10,7 +12,67 @@ namespace Nhom19_QuanLyLuuTruDienTu.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<ObjFile> ObjFiles = new List<ObjFile>();
+            foreach (string strfile in Directory.GetFiles(Server.MapPath("~/Content/Files")))
+            {
+                FileInfo fi = new FileInfo(strfile);
+                ObjFile obj = new ObjFile();
+                obj.File = fi.Name;
+                obj.Size = fi.Length;
+                obj.Type = GetFileType(fi.Extension);
+                ObjFiles.Add(obj);
+            }
+
+            return View(ObjFiles);
         }
-    }
+        public FileResult Download(string fileName) //downloading
+        {
+            string fullPath = Path.Combine(Server.MapPath("~/Content/Files"), fileName);
+            byte[] fileBytes = System.IO.File.ReadAllBytes(fullPath);
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+        private string GetFileType(string fileExtension) //file type
+        {
+            switch (fileExtension.ToLower())
+            {
+                case ".docx":
+                case ".doc":
+                    return "Microsoft Word Document";
+                case ".xlsx":
+                case ".xls":
+                    return "Microsoft Excel Document";
+                case ".txt":
+                    return "Text Document";
+                case ".jpg":
+                case ".png":
+                    return "Image";
+                default:
+                    return "Unknown";
+            }
+        }
+        [HttpPost]
+        public ActionResult Index(ObjFile doc)
+        {
+            foreach (var file in doc.files)
+            {
+
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var filePath = Path.Combine(Server.MapPath("~/Content/Files"), fileName);
+                    file.SaveAs(filePath);
+                }
+            }
+            TempData["Message"] = "files uploaded successfully";
+            return RedirectToAction("Index");
+        }
+    } 
+    
+}
+public class ObjFile //test without data
+{
+    public IEnumerable<HttpPostedFileBase> files { get; set; }
+    public string File { get; set; }
+    public long Size { get; set; }
+    public string Type { get; set; }
 }
